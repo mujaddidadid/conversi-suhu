29c586d5dc47152501499e361755dbc96da87f95
mujaddidadid <143859254+mujaddidadid@users.noreply.github.com>
2023-09-07 13:20:58 +0700

    ini tugas satu pelcod
    soal nomer 1 dan 2

---
+++ ganjil.py
@@ -0,0 +1,23 @@
+#Soal NIM ganjil nomer 1
+#Komversi suhu dari celcius ke fahrenheit
+#Rumus celcius to fahrenheit 9/5*c+32
+
+#print('Conversi suhu celcius to fahrenheit')    #rumus 9/5*c
+#celcius = int(input('masukkan suhu celcius: ')) #hasil rumus9/5*c lalu ditambah 32
+#r1 = 9/5
+#r2 = 32 
+#rumuscelciustofahrenheit = r1*celcius   
+#rumus2 = rumuscelciustofahrenheit + r2
+#print('Maka suhu fahrenheitnya adalah', rumus2,"*f")
+
+#soal NIM ganjil nomer 2
+#Konversi suhu dari fahrenheit ke celcius
+#Rumus  fahrenheit to celcius
+
+print('Conversi suhu fahrenheit to celcius')        
+fahrenheit = int(input('Masukkan suhu fahrenheit: '))    
+r1 = 5/9
+r2 = 32
+rumus2 = fahrenheit-r2
+rumusfahrenheitcelcius = r1*rumus2
+print('Maka suhu celciusnya adalah',rumusfahrenheitcelcius,'*c')
